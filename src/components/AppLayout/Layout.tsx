319979db129daa82547e5a5545f4b9d855b6d2b2
Marlen Hima <m.hima@evolutivo.it>
2022-05-26 09:40:07 +0200

    fix layout container

--- src/components/AppLayout/Layout.tsx
+++ src/components/AppLayout/Layout.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import ResponsiveAppBar from "./ResponsiveAppBar";
 import { Footer } from "./Footer";
 import '../../index.css';
-import { Box } from "@mui/material";
 
 export const Layout = (props: any) => {
   const { children } = props;
   return (
     <>
       <ResponsiveAppBar />
-      <Box px={5} >{children}</Box>
+      <Container sx={{paddingBottom:'5%'}}>{children}</Container>
       <Footer />
     </>
   );
