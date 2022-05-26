4694fc0e090f8d18ef6a7e85fa74188f54592f0c
Marlen Hima <m.hima@evolutivo.it>
2022-05-26 10:55:03 +0200

    change expired key

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-export const API_KEY :string = 'k_41bovf6a';
+export const API_KEY :string = 'k_xs8ccgba';
 export const API_ENDPOINT :string = `https://imdb-api.com/API/AdvancedSearch/${API_KEY}?groups=top_250&count=250`;
 export enum FILTER_COMPERATORS {
     CONTAINS = 'contains',
